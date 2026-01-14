d6c72ac6b8221b3d8638bca66189edde8b4e372d
Mubarak7671 <shaikmubarak63487@gmail.com>
2026-01-14 09:54:24 +0530

    Commit 1 Here i am change some of duration changes and change text color

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart(prev => {
+      const exists = prev.find(item => item.id === product.id);
+
+      if (exists) {
+        return prev.map(item =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+
+      return [
+        ...prev,
+        {
+          ...product,
+          price: product.finalPrice,   // 🔥 important fix
+          quantity: 1
+        }
+      ];
+    });
+  };
+
+  const increaseQty = (id) => {
+    setCart(prev =>
+      prev.map(item =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decreaseQty = (id) => {
+    setCart(prev =>
+      prev.map(item =>
+        item.id === id
+          ? { ...item, quantity: Math.max(1, item.quantity - 1) }
+          : item
+      )
+    );
+  };
+
+  const removeFromCart = (id) => {
+    setCart(prev => prev.filter(item => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, increaseQty, decreaseQty, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
